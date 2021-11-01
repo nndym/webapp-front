360506dea79c8939cc808c6eb6174da2bf3a271b
Vidur Ratna <vidur_ratna@me.com>
2021-11-02 12:03:37 +1300

    downgrade to next 12.0.1

---
+++ pages/_document.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Html, Head, Main, NextScript } from 'next/document'
+
+function MyDocument() {
+    return (
+        <Html>
+            <Head>
+                <link rel="preconnect" href="https://fonts.gstatic.com" />
+                <link 
+                    href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap"
+                    rel="stylesheet"
+                />
+                <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css"></link>
+            </Head>
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    )
+}
+
+export default MyDocument
