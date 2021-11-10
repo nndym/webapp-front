e17de2f52105f291d9ab3ccb272c63c3cd64c5c8
Vidur Ratna <vidur_ratna@me.com>
2021-11-11 11:10:50 +1300

    add first_name and last_name

--- components/Auth/RegisterForm.tsx
+++ components/Auth/RegisterForm.tsx
@@ -1,19 +1,47 @@
+import Button from '@components/Button'
 import EmailInput from '@components/Input/Email'
 import PasswordInput from '@components/Input/Password'
+import TextInput from '@components/Input/Text'
 import { Form, FormikProvider, useFormik } from 'formik'
 import Link from 'next/link'
 import React from 'react'
-import { EmailIcon, PasswordIcon } from 'static_data/icons'
+import { EmailIcon, NameIcon, PasswordIcon } from 'static_data/icons'
+import * as yup from 'yup';
+
+const max_date = new Date();
+      max_date.setFullYear(max_date.getFullYear() - 16);
+
+const min_date = new Date();
+      min_date.setFullYear(min_date.getFullYear() - 250);
+
+
+const RegisterFormValidation = yup.object().shape({
+    email: yup.string().email('Please enter a valid email!').required('Email is required'),
+    password: yup.string()
+                 .min(6, 'Password must be at least 6 characters long')
+                 .matches(/[0-9]/, 'Password must contain at least one number')
+                 .matches(/[a-z]/, 'Password must contain at least one lowercase letter')
+                 .matches(/[A-Z]/, 'Password must contain at least one uppercase letter')
+                 .matches(/[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/, 'Password must contain at least one special character')
+                 .required('Password is required.'),
+    first_name: yup.string().required('First name is required.'),
+    last_name: yup.string().required('Last name is required.'),
+    country: yup.string().required('Country is required.'),
+    date_of_birth: yup.date().required('Date of birth is required.')
+                             .max(max_date, 'You must be at least 16.')
+                             .min(min_date, 'Please enter a real age!'),
+})
 
 function RegisterForm() {
 
     const formik = useFormik({
+        validationSchema: RegisterFormValidation,
         initialValues: {
             email: '',
             password: '',
             username: '',
-            firstName: '',
-            lastName: '',
+            first_name: '',
+            last_name: '',
             date_of_birth: '',
             country: '',
         },
@@ -31,8 +59,26 @@ function RegisterForm() {
             <FormikProvider value={formik}>
                 <Form className='my-4'>
                     <div className="grid grid-cols-2 gap-4">
-                        <p>First Name</p>
-                        <p>Last Name</p>
+                        <TextInput
+                            error={formik.touched.first_name && formik.errors.first_name && formik.errors.first_name.toString()}
+                            value={formik.values.first_name}
+                            spacing
+                            onChange={formik.handleChange}
+                            name="first_name"
+                            autoComplete="given-name"
+                            label="First Name"
+                            icon={NameIcon}
+                        />
+                        <TextInput
+                            error={formik.touched.last_name && formik.errors.last_name && formik.errors.last_name.toString()}
+                            value={formik.values.last_name}
+                            spacing
+                            onChange={formik.handleChange}
+                            autoComplete="family-name"
+                            name="last_name"
+                            label="Last Name"
+                            icon={NameIcon}
+                        />
                     </div>
                     <div className="grid grid-cols-2 gap-4">
                         <p>Date of birth</p>
@@ -45,6 +91,7 @@ function RegisterForm() {
                         onChange={formik.handleChange}
                         name="email"
                         label="Email"
+                        autoComplete="email"
                         icon={EmailIcon}
                     />
                     <PasswordInput
@@ -55,9 +102,18 @@ function RegisterForm() {
                         icon={PasswordIcon}
                         onChange={formik.handleChange}
                         onBlur={formik.handleBlur}
+                        autoComplete='new-password'
                         value={formik.values.password}
                         error={formik.touched.password && formik.errors.password && formik.errors.password.toString()}
                     />
+                    <Button 
+                        type="submit"
+                        disabled={formik.isSubmitting}
+                        loading={formik.isSubmitting}
+                        className="w-full my-2"
+                    >
+                        Register
+                    </Button>
                 </Form>
             </FormikProvider>
         </div>
