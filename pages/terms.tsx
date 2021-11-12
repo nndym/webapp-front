acd5a7521dccdfd7c6a235b3f225d5ebcdf874e7
Vidur Ratna <vidur_ratna@me.com>
2021-11-12 15:04:59 +1300

    create pages to use footer

---
+++ pages/terms.tsx
@@ -0,0 +1,20 @@
+import MainLayout from '@components/Layout/Main'
+import Navigation from '@components/Navigation'
+import Head from 'next/head'
+import React from 'react'
+
+function Terms() {
+    return (
+        <>
+           <Head>
+                <title>Terms and conditions - NNDYM</title>
+           </Head> 
+           <Navigation/>
+           <MainLayout>
+                
+           </MainLayout>
+        </>
+    )
+}
+
+export default Terms
