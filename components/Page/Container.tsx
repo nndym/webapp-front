1a7834eb23ce0f8ada201663a94b76d3aaa1d381
Vidur Ratna <vidur_ratna@me.com>
2021-11-22 20:47:38 +1300

    add less container padding for lg

--- components/Page/Container.tsx
+++ components/Page/Container.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function PageContainer({children, half}:{children:React.ReactNode, half?:boolean}) {
     return (
-        <div className='container sm:m-auto lg:px-16 xl:px-64 px-8 py-4'>
+        <div className='container sm:m-auto md:px-12 lg:px-16 xl:px-48 px-8 py-4'>
             <div className={"grid" + (half ? " grid-cols-1 md:grid-cols-2 gap-4"  : " ")}>
                 {children}
             </div>
